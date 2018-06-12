d5574142a71590eb27dd38920ca23bf81c52e647
Cook1619 <cook6348@gmail.com>
2018-06-12 12:20:40 -0500

    My attempt at making a promise

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
 let num;
-function message(){
+function message() {
     console.log("Hello");
     num = 7;
     return num;
 }
 message();
 
-function anotherMessage(){
+function anotherMessage() {
     console.log(num * 4);
 }
 setTimeout(anotherMessage, 2000);
 
-function getWords(){
+function getWords() {
     console.log('Hello');
-    setTimeout(()=>{
+    setTimeout(() => {
         console.log('SecondWord')
     }, 2000)
-    setTimeout(()=> {
+    setTimeout(() => {
         console.log('Covalance!!')
     }, 0)
     console.log('WordyWOrd');
 }
 getWords();
 
-function countdown(num, cb){
-    for(let i = num; num < 0; i-- ){
+function countdown(num, cb) {
+    for (let i = num; num < 0; i--) {
         console.log(num);
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         cb();
     }, num)
 }
 
-function done(){
+function done() {
     console.log('message in the console');
 }
 
-countdown(5000,done);
+countdown(5000, done);
 
-// let statement = true;
+let statement = true;
 
-// let orderChickenSandwich = new Promise(function(resolve, reject){
-//   if(statement === true){
+let orderChickenSandwich = new Promise(function (resolve, reject) {
+    let food = {
+        sandwich: 'chicken',
+        veggies: 'veggies'
+    }
+    if (statement === true) {
+        resolve(food);
+    }
+    else {
+        reject(new Error('ERROR!!'));
+    }
+})
 
-//   }
-// })
