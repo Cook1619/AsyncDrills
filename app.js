8f7c79800b46068115c271048f39757d16f9eca9
Cook1619 <cook6348@gmail.com>
2018-06-12 08:37:08 -0500

    Getwords function not logging as intended

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-let num;
-function message(){
-    console.log("Hello");
-    num = 7;
-    return num;
-}
-message();
+// let num;
+// function message(){
+//     console.log("Hello");
+//     num = 7;
+//     return num;
+// }
+// message();
 
-function anotherMessage(){
-    console.log(num * 4);
+// function anotherMessage(){
+//     console.log(num * 4);
+// }
+// setTimeout(anotherMessage, 2000);
+
+function getWords(){
+    console.log('Hello');
+    setTimeout(console.log('AnotherWord'), 2000);
+    setTimeout(console.log('ThirdWord'),0);
+    console.log('WordyWOrd');
 }
-setTimeout(anotherMessage, 2000);
+getWords();
