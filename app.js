7318a3734ae73a4bd8f994f97d03a4aeaacc39d7
Cook1619 <cook6348@gmail.com>
2018-06-12 16:58:19 -0500

    Playing with different methods

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-//Async now and later
+// //Async now and later
 
-let num;
-function message() {
-    console.log("Hello");
-    num = 7;
-    return num;
-}
-message();
+// let num;
+// function message() {
+//     console.log("Hello");
+//     num = 7;
+//     return num;
+// }
+// message();
 
-function anotherMessage() {
-    console.log(num * 4);
-}
-setTimeout(anotherMessage, 2000);
+// function anotherMessage() {
+//     console.log(num * 4);
+// }
+// setTimeout(anotherMessage, 2000);
 
-//Callbacks
+// //Callbacks
 
-function getWords() {
-    console.log('Hello');
-    setTimeout(() => {
-        console.log('SecondWord')
-    }, 2000)
-    setTimeout(() => {
-        console.log('Covalance!!')
-    }, 0)
-    console.log('WordyWOrd');
-}
-getWords();
+// function getWords() {
+//     console.log('Hello');
+//     setTimeout(() => {
+//         console.log('SecondWord')
+//     }, 2000)
+//     setTimeout(() => {
+//         console.log('Covalance!!')
+//     }, 0)
+//     console.log('WordyWOrd');
+// }
+// getWords();
 
 function countdown(num, cb) {
     setTimeout(function () {
@@ -71,17 +71,20 @@ orderFood.then(function (resolve) {
 let number = 1;
 chainingPromises = () => {
     return new Promise((resolve, reject) => {
-        setTimeout(function(){
+        setTimeout(function () {
             resolve(number)
-        },2000)
-    }).then(function(res){
-        return res * 2;
-    }).then(function(res){
-        return res * 4;
-    }).then(function(res){
-        return res * 6;
-    }).then(function(res){
+        }, 2000)
+    }).then((res) => {
         console.log(res);
+        return res * 2;
+    }).then((res) => {
+        setTimeout(() => console.log(res), 2000)
+        return res * 4;
+    }).then((res) => {
+        setInterval(() => console.log(res), 4000)
+        return res * 6;
+    }).then((res) => {
+        setInterval(() => console.log(res), 6000);
     });
 }
 chainingPromises();
